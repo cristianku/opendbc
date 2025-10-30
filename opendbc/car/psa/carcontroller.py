1a93dbabcf6e92f95119558595fed048de1f77dd
cristian zantedeschi <c.zantedeschi@gmail.com>
2025-10-31 00:53:52 +0100

    c

--- opendbc/car/psa/carcontroller.py
+++ opendbc/car/psa/carcontroller.py
@@ -116,6 +116,9 @@ class CarController(CarControllerBase):
     if self.frame % 1000 == 0:
       can_sends.append(create_request_takeover(self.packer, CS.HS2_DYN_MDD_ETAT_2F6,1))
 
+    if self.frame % 2 == 0 and steer_hud_alert:
+      can_sends.append(create_request_takeover(self.packer, CS.HS2_DYN_MDD_ETAT_2F6,2))
+
     #   # 100Hz ##
     if CC.latActive:
       torque = self.driver_torque_gen.next_value()
