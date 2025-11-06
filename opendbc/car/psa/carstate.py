33d3ff81e9410d5b39e51778173a5a1a8bdacc21
cristian zantedeschi <c.zantedeschi@gmail.com>
2025-11-06 22:41:47 +0100

    c

--- opendbc/car/psa/carstate.py
+++ opendbc/car/psa/carstate.py
@@ -13,7 +13,7 @@ TransmissionType = structs.CarParams.TransmissionType
 
 class CarState(CarStateBase):
   def __init__(self, CP, CP_SP):
-    super.__init__( CP, CP_SP)
+    super().__init__(CP, CP_SP)
     self.is_dat_dira = dict()
     self.steering = dict()
     self.dyn4_fre = dict()  # Store wheel speeds for spoofing
