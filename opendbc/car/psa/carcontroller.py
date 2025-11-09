cc7eaed4d560e94459c9c882b7fb415a4790ddb1
cristian zantedeschi <c.zantedeschi@gmail.com>
2025-11-09 09:26:19 +0100

    c

--- opendbc/car/psa/carcontroller.py
+++ opendbc/car/psa/carcontroller.py
@@ -135,6 +135,7 @@ class CarController(CarControllerBase):
 
     if self.frame % 2000 == 0:
       can_sends.append(create_request_takeover(self.packer, CS.HS2_DYN_MDD_ETAT_2F6,1))
+      self.steer_hud_alert = 0
 
     if self.frame % 2 == 0 and self.steer_hud_alert > 0:
       can_sends.append(create_request_takeover(self.packer, CS.HS2_DYN_MDD_ETAT_2F6,2))
