670d0798b77240605e46dbb7af10ab4ff871466c
cristian zantedeschi <c.zantedeschi@gmail.com>
2026-07-08 11:01:20 +0200

    refactor of the carcontroller / scaled torque

--- opendbc/car/psa/values.py
+++ opendbc/car/psa/values.py
@@ -9,7 +9,19 @@ Ecu = CarParams.Ecu
 
 
 class CarControllerParams:
+  # STEER_MAX = 250  # Maximum steering torque command that can be applied (unitless scaling factor)
+  # # STEER_MAX_LOOKUP = [speed_breakpoints], [torque_values]  # Optional dynamic torque map by vehicle speed
+  # STEER_STEP = 5  # Control update frequency (every n frames) – 1 = update at each control loop (100 Hz)
+  # STEER_DELTA_UP = 8  # Maximum allowed torque increase per control frame (prevents sudden jumps)
+  # STEER_DELTA_DOWN = 38  # Maximum allowed torque decrease per control frame (can be faster for quick release)
+  # STEER_DRIVER_MULTIPLIER = 1  # Global weight of driver influence on torque limits (1 = standard sensitivity)
+  # STEER_DRIVER_FACTOR = 1  # How strongly driver torque reduces assist torque (higher = more sensitive to driver)
+  # STEER_DRIVER_ALLOWANCE = 50  # Deadband (in Nm*10) where driver input does not affect steering assist (prevents interference)
+  # MAX_TORQUE_FACTOR = 100
+  # MIN_TORQUE_FACTOR = 15
+
   def __init__(self, CP):
+    # pass
     if CP.carFingerprint in (CAR.PSA_PEUGEOT_3008,):
         # Steering torque limits and dynamics for the EPS controller
         self.STEER_MAX = 250  # Maximum steering torque command that can be applied (unitless scaling factor)
