044eac0ad6d20884d4ab9e76c9d5dd1e2b4150f6
cristian zantedeschi <c.zantedeschi@gmail.com>
2026-07-29 20:08:34 +0200

    Update takeover request condition in CarController to include lateral activation checks for improved functionality

--- opendbc/car/psa/carcontroller.py
+++ opendbc/car/psa/carcontroller.py
@@ -212,7 +212,7 @@ class CarController(CarControllerBase):
           self.apply_torque_factor = 0
           carlog.error(f"PSA_DEBUG sending empty torque apply_new_torque_scaled={apply_new_torque_scaled} ")
 
-        if self.frame % 3000 in (0, 5, 10):
+        if CC.latActive and CS.eps_active and self.frame % 3000 in (0, 5, 10):
           self.takeover_req = 1
 
         if self.apply_torque_factor > 0 and apply_new_torque_scaled != 0:
