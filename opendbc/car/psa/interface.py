04db4b25239e204e61a32fe597d801141404e200
cristian zantedeschi <c.zantedeschi@gmail.com>
2026-07-30 23:19:14 +0200

    Set dashcamOnly to True in CarInterface parameters

--- opendbc/car/psa/interface.py
+++ opendbc/car/psa/interface.py
@@ -16,7 +16,7 @@ class CarInterface(CarInterfaceBase):
     ret.safetyConfigs = [get_safety_config(structs.CarParams.SafetyModel.psa)]
 
     #
-    ret.dashcamOnly = False
+    ret.dashcamOnly = True
 
     if candidate in (CAR.PSA_PEUGEOT_3008,CAR.PSA_CITROEN_C4_SPACETOURER):
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
