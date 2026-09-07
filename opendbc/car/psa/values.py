14c4346783e66b8f4c8e5a69748874f6495547fa
cristian zantedeschi <c.zantedeschi@gmail.com>
2026-09-07 12:07:06 +0200

    DISABLE_SPEED

--- opendbc/car/psa/values.py
+++ opendbc/car/psa/values.py
@@ -112,7 +112,7 @@ class LKAS_LIMITS:
   # Peugeot 3008
   # STEER_THRESHOLD: torque (deci-Nm) to detect driver input (steeringPressed)
   # DISABLE/ENABLE_SPEED: LKA hysteresis in km/h
-  DISABLE_SPEED = 52    # kph
+  DISABLE_SPEED = 51    # kph
   ENABLE_SPEED = 51     # kph
 
 
